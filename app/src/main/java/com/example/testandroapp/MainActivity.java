3baa331570eb585c70f580427701ab7a68edca5e
Mohit Prajapati <106908866+mohitpraja@users.noreply.github.com>
2024-02-29 15:06:10 +0530

    style added

--- app/src/main/java/com/example/testandroapp/MainActivity.java
+++ app/src/main/java/com/example/testandroapp/MainActivity.java
@@ -42,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    public void checkTmp(){
+        System.out.println("this is my check temp print");
+    }
 //    public void tstClick(View view){
 //        et_mail = (EditText) findViewById(R.id.et_mail);
 //        et_password = (EditText) findViewById(R.id.et_password);
